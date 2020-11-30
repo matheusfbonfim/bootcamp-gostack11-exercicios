239e3438c5a4059430bf22e2f27f105dfdb0bf73
matheusfbonfim <matheusbonfim@alunos.utfpr.edu.br>
2020-11-30 16:33:52 -0300

    Update

---
+++ nivel-1/frontend-react/src/babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env', // Converte o codigo conforme o ambiente
+    '@babel/preset-react' // Adiciona as funcionalidades do react nessa conversão
+  ],
+}
