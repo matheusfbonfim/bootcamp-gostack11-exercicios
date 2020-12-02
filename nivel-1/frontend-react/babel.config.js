a5693ba123294a7b32ff3fc1dda4f299d90c235a
matheusfbonfim <matheusbonfim@alunos.utfpr.edu.br>
2020-12-02 16:01:36 -0300

    Delete archives

--- nivel-1/frontend-react/babel.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  presets: [
-    '@babel/preset-env', // Converte o codigo conforme o ambiente
-    '@babel/preset-react' // Adiciona as funcionalidades do react nessa conversão
-  ]
-}
