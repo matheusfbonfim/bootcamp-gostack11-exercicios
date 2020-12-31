37c1c2620fb85f7319bf057cdf219163caf69aa0
Matheus Bonfim <matheusbonfim@alunos.utfpr.edu.br>
2020-12-31 11:34:58 -0300

    Update state

--- fase-01/frontend-reactJS/src/App.js
+++ fase-01/frontend-reactJS/src/App.js
@@ -1,35 +1,47 @@
 // Importa o react 
-import React from 'react';
+import React, {useState} from 'react';
 
 // Importando o componente Header - cabeçalho
 import Header from './components/Header';
 
-/* Caso queira repetir o mesmo componente, necessário colocar uma div ou 
-   bloco por fora ou fragment (<> code aqui.. </>)
+/* 
+* Caso queira repetir o mesmo componente, necessário colocar uma div ou 
+* bloco por fora ou fragment (<> code aqui.. </>)
+*
+* Componente 
+* Propriedade - informação que pode-se passar de um componente pai para um filho
 */
 
-/**
- * Componente 
- * Propriedade - informação que pode-se passar de um componente pai para um filho
- */
-
+// Para incluir JS no componente sempre usar "{}"
+// Maneira dinamica retorna os projects -> Retorna itens da lista
+// Ex: {projects.map(project => <li>{project}</li>)}
 function App(){
+  // useState retorna um array com 2 posições
+    // 1. Variavel com seu valor inicial
+    // 2. Função para atualizarmos esse valor
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  // Função para adicionar um projeto
+    // Utilizado no onClick -> evento do botão
+  function handleAddProject(){
+    
+    // Imutabilidade
+      // Temos que criar um novo componente, e não alterar o projects
+      //projects.push(`Novo projeto ${Date.now()}`);
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    
+    console.log(projects);
+  }
+
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-
-      <Header title="Projects">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-          <li>Login</li>
-        </ul>
-      </Header>
+      <Header title="Projects"/>
+      
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+      
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
     </>
   ) ;
 }
