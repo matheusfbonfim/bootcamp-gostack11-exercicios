98f9b2707be747b192ca877488db2cb1a0ce0f34
Matheus Bonfim <matheusbonfim@alunos.utfpr.edu.br>
2021-01-09 00:11:18 -0300

    ESLint

--- fase-02/primeiro-projeto-node/src/server.ts
+++ fase-02/primeiro-projeto-node/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 
 // Instanciando o express
 const app = express();
@@ -8,5 +8,5 @@ app.get('/',(request, response)=>{
 })
 
 app.listen(3333, () => {
-  console.log("🚀 Server started on port 3333!")
+  console.log("🚀 Server started on port 3333!");
 });
