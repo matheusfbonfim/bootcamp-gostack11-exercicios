be447497af8d80b3e7087c6415fa02dc1ee7cdf3
Matheus Bonfim <matheusbonfim@alunos.utfpr.edu.br>
2021-01-11 22:59:28 -0300

    Debug

--- fase-02/primeiro-projeto-node/src/server.ts
+++ fase-02/primeiro-projeto-node/src/server.ts
@@ -1,13 +1,16 @@
-import express from "express";
+import express from 'express';
 // Importando as rotas
-import routes from "./routes";
+import routes from './routes';
 
 // Instanciando o express
 const app = express();
+
+// Aplicação entender o formato JSON nas requisições
+app.use(express.json());
 
 // Define todas as rotas dentro do app
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log("🚀 Server started on port 3333!");
+  console.log('🚀 Server started on port 3333!');
 });
