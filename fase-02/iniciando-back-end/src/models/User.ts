66e8c23da3ddb0b7891cfab2624248895cc4e42f
Matheus Bonfim <matheusbonfim@alunos.utfpr.edu.br>
2021-01-26 12:42:25 -0300

    create_migration_model_user

--- fase-02/iniciando-back-end/src/models/User.ts
+++ fase-02/iniciando-back-end/src/models/User.ts
@@ -7,22 +7,26 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
-// Criação de um modelo/entidade para o tipo appointment - agendamento
+// Criação de um modelo/entidade para o tipo appointment - user
 // Importante para ser usado externamente, como o banco de dados e nas rotas
-// Cada agendamento é descrito por essa classe
+// Cada user é descrito por essa classe
 // Uso do entity como decorator para tabela no banco de dados
-@Entity('appointments')
-class Appointment {
-  // Considerado uma chave auto_increment
+@Entity('users')
+class User {
+  // Considerado uma chave auto_increment -> uuid
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   // Definida como uma coluna varchar - default
   @Column()
-  provider: string;
+  name: string;
 
-  @Column('timestamp with time zone')
-  date: Date;
+  // Definida como uma coluna varchar - default
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
 
   // O typeorm já tem um tipo de variavel para armazenar a data de criação
   @CreateDateColumn()
@@ -32,4 +36,4 @@ class Appointment {
   update_at: Date;
 }
 
-export default Appointment;
+export default User;
